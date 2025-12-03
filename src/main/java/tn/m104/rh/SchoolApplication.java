b8154870cc40a0f0c7b06506677870219633451a
adam mediouni <140275241+ademmedyouni@users.noreply.github.com>
2025-12-03 12:25:28 +0100

    Add files via upload

---
+++ src/main/java/tn/m104/rh/SchoolApplication.java
@@ -0,0 +1,20 @@
+package tn.m104.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// test comment 1
+// test comment 2
+
+// test pull request
+
+@SpringBootApplication
+
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+}
+	
